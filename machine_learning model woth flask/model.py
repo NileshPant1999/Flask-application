72e705fc403260a7fa22a1abecd8be1cb233198b
NileshPant1999 <nileshpant6969@gmail.com>
2020-02-09 23:35:40 +0530

    First machine learning model deployment using flask

---
+++ machine_learning model woth flask/model.py
@@ -0,0 +1,24 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
+
+iris = load_iris()
+X = iris.data
+Y = iris.target
+
+X_train , X_test , Y_train, Y_test = train_test_split(X,Y, random_state=42, test_size=0.5)
+
+# Build the model_selection
+
+clf = RandomForestClassifier(n_estimators=10)
+
+#Train the classifier
+clf.fit(X_train, Y_train)
+
+# Prediction
+
+predicted = clf.predict(X_test)
+
+print(accuracy_score(predicted,Y_test))
